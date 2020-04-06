6ccc392778ffc23a529f474d7c796673436c9b10
JohnYehyo <836524969@qq.com>
2020-04-06 21:55:58 +0800

    配置打包的入口和出口

--- 11webpack/webpack.config.js
+++ 11webpack/webpack.config.js
@@ -1,4 +1,14 @@
+const path = require('path')
+
 module.exports = {
     //编译模式(development、production)
-    mode: 'development'
+    mode: 'development',
+    //打包的入口文件
+    entry: path.join(__dirname, './src/index.js'),
+    output:{
+        //输出文件存放路径
+        path: path.join(__dirname, './dist'),
+        //输出文件的名称
+        filename: 'bundle.js'
+    }
 }
