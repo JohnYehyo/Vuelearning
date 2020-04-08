9a00e30f106d8223155d7e87afa4cab6c71c349f
JohnYehyo <836524969@qq.com>
2020-04-08 22:13:10 +0800

    单文件组件

---
+++ 11webpack/src/components/app.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>VUE单文件组件</h1>
+    </div>
+
+</template>
+
+<script>
+export default {
+    data(){
+        return{};
+    },
+    methods:{}    
+}
+</script>
+
+<style scoped>
+    h1{
+        color: darkseagreen;
+    }
+</style>
